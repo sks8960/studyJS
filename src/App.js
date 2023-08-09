c51f52b595a09c20f074276c33eea77c8d0a38a9
sks8960 <sks8960@naver.com>
2023-08-09 18:13:29 +0900

    다이어리 기본 기능 제작

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import React, {useRef, useEffect} from 'react';
 import './App.css';
+import DiaryEditor from "./DiaryEditor";
+import DiaryList from './DiaryList';
 
-function App() {
+const dummyList = [
+  {
+    id:1,
+    author:"신경식",
+    content:"하이 1",
+    emotion:5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id:2,
+    author:"이순신",
+    content:"하이 2",
+    emotion:4,
+    created_date: new Date().getTime(),
+  },
+  {
+    id:3,
+    author:"홍길동",
+    content:"하이 3",
+    emotion:3,
+    created_date: new Date().getTime(),
+  },
+]
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <DiaryEditor/>
+      <DiaryList diaryList={dummyList}/>
     </div>
   );
 }
