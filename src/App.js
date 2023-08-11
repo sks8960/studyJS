1a47530093201641e50ec2eee1cf125829ee635d
sks8960 <sks8960@naver.com>
2023-08-11 16:56:31 +0900

    라이프사이클 활용 공부

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {useRef, useState} from 'react';
 import './App.css';
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from './DiaryList';
+import Lifecycle from './Lifecycle';
 
 const App = () => {
 
@@ -33,6 +34,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <Lifecycle />
       <DiaryEditor onCreate={onCreate}/>
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data}/>
     </div>
