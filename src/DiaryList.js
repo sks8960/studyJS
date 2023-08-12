b1345636ce5a73d9ca1313dce565ae83b3cc40ed
sks8960 <sks8960@naver.com>
2023-08-12 15:54:23 +0900

    useMemo를 사용한 연산 최적화

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,7 +1,6 @@
 import DiaryItem from "./DiaryItem";
 
 const DiaryList = ({onEdit, onRemove, diaryList}) => {
-    console.log(diaryList);
     return <div className="DiaryList">
         <h2>일기 리스트</h2>
         <h4>{diaryList.length}개의 일기가 있습니다.</h4>
